36bb763e375e25646248764e27af7cb6fa046818
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-25 22:03:03 +0300

    leetcode

--- src/main/java/org/example/top_interviewQA/Chare.java
+++ src/main/java/org/example/top_interviewQA/Chare.java
@@ -3,7 +3,13 @@ package org.example.top_interviewQA;
 // Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
 public class Chare {
     public int titleToNumber(String columnTitle) {
-        int value = 0;for (int i = 0; i < columnTitle.length(); i++) {int result = columnTitle.charAt(i) - 64;value += Math.pow(26, columnTitle.length() - i - 1) * result;}return value;
+
+        int value = 0;for (int i = 0; i < columnTitle.length(); i++) {
+            int result = columnTitle.charAt(i) - 64;
+            value += Math.pow(26, columnTitle.length() - i - 1) * result;
+        }
+        return value;
+
     }
 
     public static void main(String[] args) {
